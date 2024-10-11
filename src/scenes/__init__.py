24b2e15f4c562c4951c4efb2550b973464e34999
NikitaSiulin <n.siulin@rufago.ru>
2024-10-11 23:32:23 +0700

    feat: common scenes

--- src/scenes/__init__.py
+++ src/scenes/__init__.py
@@ -1,3 +1,5 @@
-all_scenes = []
+from scenes.common import CancellableScene, DefaultScene, LanguageScene, LikeBotsScene
+
+all_scenes = [CancellableScene, DefaultScene, LanguageScene, LikeBotsScene]
 
 __all__ = ["all_scenes"]
